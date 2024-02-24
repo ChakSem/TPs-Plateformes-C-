5a0ed32fd009a45b5a255d863aa1604841698a80
ChakSem <alaa.chakori.semmane@gmail.com>
2024-02-24 19:03:12 +0100

    pop up qui permet de notifier l'id du premier utilisateur

--- FrameworkQT/src/main/c++/fr/univtours/polytech/FrameworkQT/view/firstuserinscription.cpp
+++ FrameworkQT/src/main/c++/fr/univtours/polytech/FrameworkQT/view/firstuserinscription.cpp
@@ -2,11 +2,13 @@
 #include "ui_firstuserinscription.h"
 #include "../controller/controller.h"
 #include "../utils/exception.h"
+#include "../model/user.h"
 #include "../view/mainwindow.h"
+#include <QMessageBox>
+
 
 FirstUserInscription::FirstUserInscription(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::FirstUserInscription)
+    : QWidget(parent), ui(new Ui::FirstUserInscription)
 {
     ui->setupUi(this);
     connect(ui->pushButton, &QPushButton::clicked, this, &FirstUserInscription::actionAddUser);
@@ -17,6 +19,7 @@ FirstUserInscription::~FirstUserInscription()
     delete ui;
 }
 
+
 void FirstUserInscription::actionAddUser()
 {
     try
@@ -24,16 +27,19 @@ void FirstUserInscription::actionAddUser()
         QString nom = ui->lastNameUser->text();
         QString prenom = ui->firstNameUser->text();
         QString mdp = ui->passwordLogin->text();
+        User *User ;
         unsigned int roleValue = ROLE_USER;
         if (nom.isEmpty() || prenom.isEmpty() || mdp.isEmpty())
         {
             throw new Exception(ERREUR_ALL_TOUS_LES_CHAMPS_NE_SONT_PAS_REMPLIS);
         }
-        Controller::createUser(prenom, nom, mdp, roleValue);
+        User = Controller::createUser(nom, prenom, mdp, roleValue);
+        QMessageBox::information(this, "Information", "Votre identifiant est : " + User->getId() + " \nVeuillez le conserver précieusement");
 
         MainWindow *mainWindow = MainWindow::accessToParent(this);
 
-        if (mainWindow != NULL) {
+        if (mainWindow != NULL)
+        {
             mainWindow->openConnection();
         }
     }
@@ -41,4 +47,5 @@ void FirstUserInscription::actionAddUser()
     {
         e->EXCAffichageErreur();
     }
+
 }
